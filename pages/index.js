f1303f305db4ddbeef99740b1020aa4c3b35b4f6
worachet raphukhiew <=>
2021-03-24 22:25:54 +0700

    init project

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import AdminHeader from '../component/layout/admin.header';
 import AdminSiderbar from '../component/layout/admin.menu';
 import AdminFooter from '../component/layout/admin.footer';
-
+/* สวัสดี index */
 export default function Home() {
   return (
   <div>
