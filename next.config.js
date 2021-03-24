3f47c906bafe4b4f4f1162edc8a175941ff1eaad
worachet raphukhiew <=>
2021-03-24 22:17:08 +0700

    init project

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const withSass = require ('@zeit/next-sass');
+
+module.exports = withSass({
+    webpack: config =>{
+        config.node = {
+            fs: 'empty'
+        }
+        return config
+    }
+})
---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, {Html,Head,Main,NextScript} from "next/document";
+export default class Mydocument extends Document{
+    render() {
+        return(
+            <Html>
+                <Head>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/css/adminlte.min.css" />
+                </Head>
+                <body className="hold-transition sidebar-mini">
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
